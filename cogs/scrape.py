bd5990f6dbc431c7eaa6366f3f97427e2f9ac78f
Prof1611 <144668269+Prof1611@users.noreply.github.com>
2025-03-22 17:25:45 +0000

    fix 3

--- cogs/scrape.py
+++ cogs/scrape.py
@@ -79,6 +79,7 @@ class Scrape(commands.Cog):
         chrome_options.add_argument("--window-position=-2400,-2400")
         chrome_options.add_argument("--log-level=3")
         chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--remote-debugging-port=9222")
 
